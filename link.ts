49758001a15bcdf6f67f18a902187ecce8e0722d
abetaev <a@betaev.pub>
2021-03-24 22:09:23 -0500

    io link

--- link.ts
+++ link.ts
@@ -8,11 +8,11 @@ import { Peer } from './types.ts';
  * 
  * @return pair of peers which are gated relatively to each other
  */
-export default function <T>(): [Peer<T>, Peer<T>] {
-  const node = peer<T>()
+export default function <I, O = I>(): [Peer<I>, Peer<O>] {
+  const node = peer<I | O>()
 
-  const first = gate(node)
-  const second = gate(node)
+  const first = gate<I>(node as Peer<I>)
+  const second = gate<O>(node as Peer<O>)
 
   return [first, second]
 }
